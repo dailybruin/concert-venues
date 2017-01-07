ea1e2195f0248ad903941b7298e831b986382834
Howard Huang <howardhuang96@gmail.com>
2017-01-07 13:01:04 -0800

    concert list templatized

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
-
+  console.log("preparing ajax call");
   $.ajax({
 		    dataType: "json",
 		    url: "https://spreadsheets.google.com/feeds/list/1s3ljuEdN7Gbm-Tqur338zYL49GMY96aIH4KF6Z4g9wE/od6/public/values?alt=json",
 				success: function(data) {
+            console.log(data.feed.entry[0]);
 						data = data.feed.entry;
 
 						// Grab the template script
-						var theTemplateScript = $("#modal-template").html();
+						var theTemplateScript = $("#list-template").html();
 
 						// Compile the template
 						var theTemplate = Handlebars.compile(theTemplateScript);
@@ -18,8 +19,8 @@ $(document).ready(function(){
 						var theCompiledHtml = theTemplate(mydata);
 
 						// Add the compiled html to the page
-						$('.modal-placeholder').html(theCompiledHtml);
+						$('.list-placeholder').html(theCompiledHtml);
 				}
 	});
 
-}
+});
